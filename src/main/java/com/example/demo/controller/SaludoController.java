9975b84e4cc3e4cf015c958d8945376e5f506dfc
Jhanxs <castro.serrano.hans@gmail.com>
2026-09-19 17:57:04 -0500

    subiendo ruta

--- src/main/java/com/example/demo/controller/SaludoController.java
+++ src/main/java/com/example/demo/controller/SaludoController.java
@@ -6,13 +6,13 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/api/v1/hans-castro")
 public class SaludoController {
 
     // Endpoint básico: Se accede entrando a http://localhost:8080/api/saludo
     @GetMapping("/saludo")
     public String enviarSaludo() {
-        return "¡Hola! Este es un saludo desde el backend en Spring Boot.";
+        return "¡Hola! Endpoint funcional.";
     }
 
     // Endpoint con parámetro: Se accede entrando a http://localhost:8080/api/saludo-personalizado?nombre=Hans
